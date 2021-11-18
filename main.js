a84a6ef29ed8dd47af5e85be2b91e176f5fcbc85
Tanja Karius <tanja.karius@googlemail.com>
2021-11-18 15:27:17 +0100

    Flex behaviour task input and single tasks

--- main.js
+++ main.js
@@ -113,8 +113,7 @@ function editTODO(todoID){
 }
 // Toggle status todo
 function statusTODO(todoID) {
-    console.log(todoID)
-    
+    // console.log(todoID)
     todoArray.forEach(el => {
         if(el.id == todoID) {
             el.todoStatus = !el.todoStatus
@@ -152,7 +151,7 @@ function updateScreen() {
         <li class="todo selector-${el.id}" id="${el.id}">
         <span id="${el.id}" class="check check-${el.id} ${el.todoStatus ? "complete" : ""}"></span>
         
-        ${el.id == editMode ? `<span id="${el.id}" class="todotext todotext-${el.id} edit" contenteditable> ${el.text}</span>` : `<span id="${el.id}" class="todotext todotext-${el.id} ${el.todoStatus ? "todo--done" : ""}"> ${el.text} is due by: <span class="todo--date">${el.dueDate}</span></span>`}
+        ${el.id == editMode ? `<span id="${el.id}" class="todotext todotext-${el.id} edit" contenteditable> ${el.text}</span>` : `<span id="${el.id}" class="todotext todotext-${el.id} ${el.todoStatus ? "todo--done" : ""}"> ${el.text}<br><span class="todo--date">${el.dueDate}</span></span>`}
         
         ${el.id == editMode ? `<button class="todo-edit" id="${el.id}"><svg id="${el.id} xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Save</title><path d="M380.93 57.37A32 32 0 00358.3 48H94.22A46.21 46.21 0 0048 94.22v323.56A46.21 46.21 0 0094.22 464h323.56A46.36 46.36 0 00464 417.78V153.7a32 32 0 00-9.37-22.63zM256 416a64 64 0 1164-64 63.92 63.92 0 01-64 64zm48-224H112a16 16 0 01-16-16v-64a16 16 0 0116-16h192a16 16 0 0116 16v64a16 16 0 01-16 16z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg></button>` : `<button class="todo-edit" id ="${el.id}"><svg id ="${el.id}" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Create</title><path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M459.94 53.25a16.06 16.06 0 00-23.22-.56L424.35 65a8 8 0 000 11.31l11.34 11.32a8 8 0 0011.34 0l12.06-12c6.1-6.09 6.67-16.01.85-22.38zM399.34 90L218.82 270.2a9 9 0 00-2.31 3.93L208.16 299a3.91 3.91 0 004.86 4.86l24.85-8.35a9 9 0 003.93-2.31L422 112.66a9 9 0 000-12.66l-9.95-10a9 9 0 00-12.71 0z"/></svg></button>`}
         
